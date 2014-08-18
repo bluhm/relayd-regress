46229f352234f656f5265de2c092cf0b0c5d5fac
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-08-19 00:43:41 +0200

    Bump copyright year.

--- Remote.pm
+++ Remote.pm
@@ -1,6 +1,6 @@
 #	$OpenBSD: Remote.pm,v 1.4 2014/07/11 16:13:11 bluhm Exp $
 
-# Copyright (c) 2010-2013 Alexander Bluhm <bluhm@openbsd.org>
+# Copyright (c) 2010-2014 Alexander Bluhm <bluhm@openbsd.org>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
