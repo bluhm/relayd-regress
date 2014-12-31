d5c2af472df44a5384f221ab84fe596b624de2b4
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-12-31 02:11:10 +0100

    Bump copyright.

--- Server.pm
+++ Server.pm
@@ -1,6 +1,6 @@
 #	$OpenBSD: Server.pm,v 1.6 2014/07/10 10:19:06 bluhm Exp $
 
-# Copyright (c) 2010-2012 Alexander Bluhm <bluhm@openbsd.org>
+# Copyright (c) 2010-2014 Alexander Bluhm <bluhm@openbsd.org>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
