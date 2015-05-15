7e68050df0008750631ad2c2e2f41ea5ca755750
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-05-15 17:38:39 +0200

    Client writes two PUT requests in a single transfer.

--- args-http-headline-callback.pl
+++ args-http-headline-callback.pl
@@ -5,12 +5,31 @@
 use strict;
 use warnings;
 
+my @lengths = (4, 3);
 our %args = (
     client => {
 	func => sub {
-	    print "GET /index.html HTTP\nHost: foo.example.com";  # no version
+	    my $self = shift;
+	    print <<'EOF';
+PUT /server-check-content-length-4.html HTTP/1.1
+Content-Length: 4
+Host: foo.bar
+
+123
+PUT /server-check-content-length-3.html HTTP/1.1
+Host: foo.bar
+Content-Length: 3
+
+12
+EOF
+	    print STDERR "LEN: 4\n";
+	    print STDERR "LEN: 3\n";
+	    http_response($self, "without len");
+	    http_response($self, "without len");
 	},
-	nocheck => 1,
+	http_vers => ["1.1"],
+	lengths => \@lengths,
+	method => "PUT",
     },
     relayd => {
 	protocol => [ "http",
@@ -18,13 +37,13 @@ our %args = (
 	    "match response header log bar",
 	],
 	loggrep => {
-	    qr/session 1 .*, done/ => 1,
+	    qr/, (?:done|last write \(done\), PUT)/ => 1,
 	},
     },
     server => {
-	noserver => 1,
-	nocheck => 1,
+	func => \&http_server,
     },
+    lengths => \@lengths,
 );
 
 1;
