677874c69ecb43722784b984327034018c515c07
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-06-25 21:30:22 +0200

    cvs commit

--- Remote.pm
+++ Remote.pm
@@ -1,4 +1,4 @@
-#	$OpenBSD: Remote.pm,v 1.5 2014/08/18 22:58:19 bluhm Exp $
+#	$OpenBSD: Remote.pm,v 1.6 2015/06/25 19:29:57 bluhm Exp $
 
 # Copyright (c) 2010-2014 Alexander Bluhm <bluhm@openbsd.org>
 #
