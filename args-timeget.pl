ebce2ea04d21b2b5cf686cc89653b44bd8c3179c
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-01-01 18:02:20 +0100

    2 seconds timeout must not occur while server writes for 4 seconds.

--- args-timeget.pl
+++ args-timeget.pl
@@ -1,17 +1,20 @@
-# test that 2 seconds timeout does not occur while client writes for 4 seconds
+# test that 2 seconds timeout does not occur while server writes for 4 seconds
 
 use strict;
 use warnings;
 
 our %args = (
     client => {
-	func => \&write_char,
-	len => 5,
-	sleep => 1,
+	func => \&read_char,
 	timefile => "",
     },
     relayd => {
 	relay => [ "session timeout 2" ],
+    },
+    server => {
+	func => \&write_char,
+	len => 5,
+	sleep => 1,
     },
     len => 5,
 );
