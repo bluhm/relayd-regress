e54b3ac02fffe86ddfef3801b86e11bbe51c19e7
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-01-04 22:42:17 +0100

    Copy timeout functions to ssl.

---
+++ args-timeget-ssl.pl
@@ -0,0 +1,22 @@
+# test that 2 seconds timeout does not occur while server writes for 4 seconds
+
+use strict;
+use warnings;
+
+our %args = (
+    client => {
+	func => \&read_char,
+	timefile => "",
+    },
+    relayd => {
+	relay => [ "session timeout 2" ],
+    },
+    server => {
+	func => \&write_char,
+	len => 5,
+	sleep => 1,
+    },
+    len => 5,
+);
+
+1;
